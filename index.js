cdbb7d19a12b05f4db18843ff8419e73625020fb
edonfy <edonfy@gmail.com>
2024-03-19 23:29:32 +0800

    feat: change parser

--- index.js
+++ index.js
@@ -1,14 +1,20 @@
 /* eslint-env node */
 
 module.exports = {
+    parser: '@babel/eslint-parser',
     extends: [
-        'eslint:recommended'
+        'eslint:recommended',
     ],
+    parserOptions: {
+        requireConfigFile: false,
+    },
     rules: {
         // base
         'no-debugger': process.env.NODE_ENV === 'production' ? 2 : 0,
         'no-console': process.env.NODE_ENV === 'production' ? 1 : 0,
         'quote-props': ['error', 'as-needed'],
-        'no-unused-vars': 'off',
-    }
+        'no-unused-vars': 1,
+        'comma-dangle': ['error', 'only-multiline'],
+        quotes: ['error', 'single'],
+    },
 }
